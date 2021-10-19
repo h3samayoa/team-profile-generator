864c54efbf890b782fe691c82d0c69564744f891
h3samayoa <h3samayoa@gmail.com>
2021-10-18 22:09:31 -0700

    added questions for manager position

--- index.js
+++ index.js
@@ -6,5 +6,62 @@ import Engineer from './lib/Engineer';
 import fs from 'fs';
 import inquirer from 'inquirer';
 
+const arr = [];
 
+const importManager = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: "Input in the Manager's name: ",
+            validate: input => {
+                if (input.length == 0) {
+                    console.log('Enter a name!');
+                    return false;
+                } else {
+                    return true;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: "Input in the Manager's Id: ",
+            validate: input => {
+                if (isNaN(input)) {
+                    console.log('enter a number!')
+                    return false;
+                } else {
+                    return true;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: "Input the Manager's email: ",
+            validate: input => {
+                input = value.match(/^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i);
+                if (input) {
+                    return true;
+                } else {
+                    return 'Enter a valid email!';
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'officeNumber',
+            message: "Input the Manager's Officer Numbers: ",
+            validate: input => {
+                if (isNaN(input)) {
+                    console.log('Enter a number!');
+                    return false;
+                } else {
+                    return true;
+                }
+            }
+        }
+    ])
+}
 
