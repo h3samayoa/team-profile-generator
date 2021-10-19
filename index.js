bca77fbae3e65cb5d3a98cb41493828b525d8640
h3samayoa <h3samayoa@gmail.com>
2021-10-18 20:12:13 -0700

    added required dependecies to index file

---
+++ index.js
@@ -0,0 +1,10 @@
+import gen from './src/gen';
+import Manager from './lib/Manager';
+import Intern from './lib/Intern';
+import Engineer from './lib/Engineer';
+
+import fs from 'fs';
+import inquirer from 'inquirer';
+
+
+
